e6f1c9bfa09b67d136116931e7ec427417398ecb
infiniteverma <vermaanant25@gmail.com>
2022-01-31 20:22:15 +0530

    change filter city to aggregate method

--- src/middlewares/filtercity.ts
+++ src/middlewares/filtercity.ts
@@ -1,18 +1,30 @@
 import {RequestHandler} from 'express';
-import {CustomerModel} from '../models/customer';
 import {ShippingModel} from '../models/shipping';
+
+const QUERY = (city: string) => [
+  {
+    $match: {city: city},
+  },
+  {
+    $lookup: {
+      from: 'customers',
+      localField: 'customerID',
+      foreignField: 'customerID',
+      as: 'data',
+    },
+  },
+  {
+    $unwind: '$data',
+  },
+  {
+    $replaceRoot: {newRoot: '$data'},
+  },
+];
 
 const filterCity: RequestHandler = async (req, res, next) => {
   let data: string[] = [];
   try {
-    const ids = await ShippingModel.find(
-      {city: req.params.city},
-      {customerID: 1},
-    ).lean();
-
-    data = await CustomerModel.find({
-      customerID: {$in: ids.map((id) => id.customerID)},
-    }).lean();
+    data = await ShippingModel.aggregate(QUERY(req.params.city));
   } catch (e) {
     return res.status(500).json({msg: e.message});
   }
