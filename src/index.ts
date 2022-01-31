57700940e511f17defadb5e864743eaa0c31b348
infiniteverma <vermaanant25@gmail.com>
2022-01-31 13:24:46 +0530

    init folder + setup express

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+// check env variables first
+if (!process.env.PORT) {
+  console.log('Missing port in .env');
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(morgan('common'));
+app.use(cors());
+
+app.use('/', (req, res) => {
+  res.json({msg: 'ok'});
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
